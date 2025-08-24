d7ff8dc54b1339e86ec69bb494fbfb577dcf69ff
user <user@ex.com>
2025-08-25 03:23:38 +0330

    ADD: add Explore screen

---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,267 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/components/bottom_navigationbar_component.dart';
+import 'package:instagram/constants/app_color_constant.dart';
+import 'package:instagram/widgets/icon_button_widget.dart';
+import 'package:instagram/widgets/icon_widget.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class ExploreScreen extends StatelessWidget {
+  const ExploreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        automaticallyImplyLeading: false,
+        title: Container(
+          width: double.infinity,
+          height: 48,
+          padding: EdgeInsetsDirectional.symmetric(
+            horizontal: 24,
+            vertical: 12,
+          ),
+          decoration: BoxDecoration(
+            border: Border.all(color: Color(AppColorConstant.naturalGray)),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              IconWidget(src: "assets/icons/linear/search.svg"),
+              SizedBox(width: 12),
+              Expanded(
+                child: TextField(
+                  style: TextStyle(color: Color(AppColorConstant.naturalGray)),
+                  decoration: InputDecoration(
+                    hintText: 'Search',
+                    hintStyle: TextStyle(
+                      color: Color(AppColorConstant.naturalGray),
+                      fontSize: 14,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                  keyboardType: TextInputType.text,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: StaggeredGrid.count(
+            crossAxisCount: 3,
+            mainAxisSpacing: 8,
+            crossAxisSpacing: 8,
+            children: [
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.asset(
+                    "assets/images/explore/post.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 2,
+                mainAxisCellCount: 2,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.asset(
+                    "assets/images/explore/post10.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.asset(
+                    "assets/images/explore/post8.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.asset(
+                    "assets/images/explore/post11.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.asset(
+                    "assets/images/explore/post1.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.asset(
+                    "assets/images/explore/post9.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.asset(
+                    "assets/images/explore/post2.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.asset(
+                    "assets/images/explore/post4.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.asset(
+                    "assets/images/explore/post3.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 2,
+                mainAxisCellCount: 2,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.asset(
+                    "assets/images/explore/post5.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.asset(
+                    "assets/images/explore/post6.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.asset(
+                    "assets/images/explore/post7.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationbarComponent(
+        children: <IconButtonWidget>[
+          IconButtonWidget(
+            icon: IconWidget(src: "assets/icons/linear/home.svg"),
+          ),
+          IconButtonWidget(
+            icon: IconWidget(
+              src: "assets/icons/bold/search.svg",
+              color: Color(AppColorConstant.accent),
+            ),
+          ),
+          IconButtonWidget(
+            icon: IconWidget(src: "assets/icons/linear/add.svg"),
+          ),
+          IconButtonWidget(
+            icon: IconWidget(src: "assets/icons/linear/heart.svg"),
+          ),
+          IconButtonWidget(
+            icon: IconWidget(src: "assets/icons/linear/profile.svg"),
+          ),
+        ],
+      ),
+    );
+  }
+}
