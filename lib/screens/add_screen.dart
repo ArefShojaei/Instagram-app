17b1bc2062f9869e9e250df702aed4adb5c8cd50
ArefShojaei <arefshojaei82dev@gmail.com>
2025-08-26 02:22:15 +0330

    ADD: add Add screen

---
+++ lib/screens/add_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/components/bottom_navigationbar_component.dart';
+import 'package:instagram/constants/app_color_constant.dart';
+import 'package:instagram/widgets/icon_button_widget.dart';
+import 'package:instagram/widgets/icon_widget.dart';
+
+class AddScreen extends StatelessWidget {
+  const AddScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        leadingWidth: 0,
+        automaticallyImplyLeading: false,
+        title: Text(
+          "Add",
+          style: TextStyle(
+            color: Color(AppColorConstant.naturalWhite),
+            fontSize: 14,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          "Soon...",
+          style: TextStyle(color: Color(AppColorConstant.naturalWhite)),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationbarComponent(
+        children: <IconButtonWidget>[
+          IconButtonWidget(
+            icon: IconWidget(src: "assets/icons/linear/home.svg"),
+          ),
+          IconButtonWidget(
+            icon: IconWidget(src: "assets/icons/linear/search.svg"),
+          ),
+          IconButtonWidget(
+            icon: IconWidget(
+              src: "assets/icons/bold/add.svg",
+              color: Color(AppColorConstant.accent),
+            ),
+          ),
+          IconButtonWidget(
+            icon: IconWidget(src: "assets/icons/linear/heart.svg"),
+          ),
+          IconButtonWidget(
+            icon: IconWidget(src: "assets/icons/linear/profile.svg"),
+          ),
+        ],
+      ),
+    );
+  }
+}
