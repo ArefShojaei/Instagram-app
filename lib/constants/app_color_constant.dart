53a90c60978af02684773dd6725d3726b9c84cc4
ArefShojaei <arefshojaei82dev@gmail.com>
2025-08-27 04:10:29 +0330

    UPDATE: update App color constants

--- lib/constants/app_color_constant.dart
+++ lib/constants/app_color_constant.dart
@@ -3,9 +3,7 @@ class AppColorConstant {
     static const int secondry = 0xFF1F2125;
     static const int secondry2 = 0xFF3E434B;
     static const int accent = 0xFFFECE00;
-    static const int naturalWhite = 0xFFFFFFFF;
-    static const int naturalGray = 0xFFC2C3CA;
-
-    static const int like = 0xFFE43636;
-    static const int link = 0xFF6484A9;
+    static const int white = 0xFFFFFFFF;
+    static const int gray = 0xFFC2C3CA;
+    static const int red = 0xFFE43636;
 }
