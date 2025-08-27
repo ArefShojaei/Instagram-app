2418159c92af94581483c16fd86c0db5b680295c
ArefShojaei <arefshojaei82dev@gmail.com>
2025-08-27 22:35:13 +0330

    REFACTOR: refactor BottomNavigationBar component

--- lib/components/bottom_navigationbar_component.dart
+++ lib/components/bottom_navigationbar_component.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:glassmorphism/glassmorphism.dart';
 import 'package:instagram/constants/app_color_constant.dart';
+import 'package:instagram/enums/page_route_enum.dart';
 import 'package:instagram/widgets/icon_button_widget.dart';
+import 'package:instagram/widgets/icon_widget.dart';
 
-class BottomNavigationbarComponent extends StatelessWidget {
-  final List<IconButtonWidget> children;
+class BottomNavigationbarComponent extends StatefulWidget {
+  final int activePageIndex;
 
-  const BottomNavigationbarComponent({super.key, required this.children});
+  const BottomNavigationbarComponent({
+    super.key,
+    required this.activePageIndex,
+  });
+
+  @override
+  State<BottomNavigationbarComponent> createState() =>
+      _BottomNavigationbarComponentState();
+}
+
+class _BottomNavigationbarComponentState
+    extends State<BottomNavigationbarComponent> {
+  late int _selectedPageIndex;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selectedPageIndex = widget.activePageIndex;
+  }
+
+  final List<String> _inActiveIcons = [
+    "assets/icons/linear/home.svg",
+    "assets/icons/linear/search.svg",
+    "assets/icons/linear/add.svg",
+    "assets/icons/linear/heart.svg",
+    "assets/icons/linear/profile.svg",
+  ];
+
+  final List<String> _activeIcons = [
+    "assets/icons/bold/home.svg",
+    "assets/icons/bold/search.svg",
+    "assets/icons/bold/add.svg",
+    "assets/icons/bold/heart.svg",
+    "assets/icons/bold/profile.svg",
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -36,9 +73,70 @@ class BottomNavigationbarComponent extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(20, 12, 20, 20),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: children,
+          children: [
+            IconButtonWidget(
+              icon: IconWidget(
+                src: _getIconPath(PageRouteEnum.home.index),
+                color: _getIconColor(PageRouteEnum.home.index),
+              ),
+              onClick: () => Navigator.pushReplacementNamed(
+                context,
+                PageRouteEnum.home.routeKey,
+              ),
+            ),
+            IconButtonWidget(
+              icon: IconWidget(
+                src: _getIconPath(PageRouteEnum.explore.index),
+                color: _getIconColor(PageRouteEnum.explore.index),
+              ),
+              onClick: () => Navigator.pushReplacementNamed(
+                context,
+                PageRouteEnum.explore.routeKey,
+              ),
+            ),
+            IconButtonWidget(
+              icon: IconWidget(
+                src: _getIconPath(PageRouteEnum.add.index),
+                color: _getIconColor(PageRouteEnum.add.index),
+              ),
+              onClick: () => Navigator.pushReplacementNamed(
+                context,
+                PageRouteEnum.add.routeKey,
+              ),
+            ),
+            IconButtonWidget(
+              icon: IconWidget(
+                src: _getIconPath(PageRouteEnum.activity.index),
+                color: _getIconColor(PageRouteEnum.activity.index),
+              ),
+              onClick: () => Navigator.pushReplacementNamed(
+                context,
+                PageRouteEnum.activity.routeKey,
+              ),
+            ),
+            IconButtonWidget(
+              icon: IconWidget(
+                src: _getIconPath(PageRouteEnum.profile.index),
+                color: _getIconColor(PageRouteEnum.profile.index),
+              ),
+              onClick: () => Navigator.pushReplacementNamed(
+                context,
+                PageRouteEnum.profile.routeKey,
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  String _getIconPath(int index) {
+    return _selectedPageIndex == index
+        ? _activeIcons[index]
+        : _inActiveIcons[index];
+  }
+
+  Color? _getIconColor(int index) {
+    return _selectedPageIndex == index ? Color(AppColorConstant.accent) : null;
   }
 }
